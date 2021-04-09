5b9e45a01987b2458cf468f55feed89ef0e241d5
Alexander Shirokov <alieksandr_shirokov@list.ru>
2021-04-09 15:51:03 +0300

    All to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-#include "task1.h"
 using namespace std;
+
+unsigned int getPrime(int value)
+{
+	int sum = 0;
+	for (int i = 3; i < value; i++)
+	{
+		if (isPrime(i) && (i % 3 == 0 || i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	return sum;
+}
+static bool isPrime(int n)
+{
+	for (int i = 2; i <= n / 2; i++)
+	{
+		if ((n % i) == 0)
+			return false;
+	}
+	return true;
+}
 
 int main()
 {
